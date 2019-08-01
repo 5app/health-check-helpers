7603656298b5893cc3db94730e0afee78fcf85b3
cboyce183 <wxaz020@live.rhul.ac.uk>
2019-08-01 14:07:27 +0100

    naming

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const kueStats = require('./lib/kueStats');
+const bullStats = require('./lib/bullStats');
 const processMetadata = require('./lib/processMetadata');
 
 module.exports = {
-	kueStats,
+	bullStats,
 	processMetadata,
 };
