4b18f3d489ad13418333bb20a79d454d83a2907a
Charles Boyce <31250541+cboyce183@users.noreply.github.com>
2019-09-05 11:15:46 +0100

    feat: Add support for Bull (#2)
    * WIP update to bull
    
    * naming
    
    * naming
    
    * Update index.js
    
    * Update index.js
    
    * Create kueStats.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const kueStats = require('./lib/kueStats');
+const bullStats = require('./lib/bullStats');
 const processMetadata = require('./lib/processMetadata');
 
 module.exports = {
 	kueStats,
+	bullStats,
 	processMetadata,
 };
