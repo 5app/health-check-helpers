3e8f1252f8e8a7d8219bad3d68d8fdc93f29047e
Iheb KHEMISSI <iheb.khemissi@gmail.com>
2019-12-05 17:00:51 +0000

    feat: Add simple health check server and client
    Provide a simple server and client (node script) to simplify adding health check support in microservices.
    
    noissue

---
+++ bin/check.js
@@ -0,0 +1,39 @@
+const http = require('http');
+
+const {
+	HEALTHCHECK_PORT = 9999,
+	HEALTHCHECK_HOST = 'localhost',
+	HEALTHCHECK_TIMEOUT = 5000, // In milliseconds
+} = process.env;
+
+const options = {
+	host: HEALTHCHECK_HOST,
+	port: HEALTHCHECK_PORT,
+	timeout: HEALTHCHECK_TIMEOUT,
+};
+
+const request = http.request(options, res => {
+	if (res.statusCode === 200) {
+		let body = '';
+
+		res.setEncoding('utf8');
+		res.on('data', chunk => {
+			body += chunk;
+		});
+
+		res.on('end', () => {
+			console.log(body); // eslint-disable-line no-console
+			process.exit(0);
+		});
+	}
+	else {
+		process.exit(1);
+	}
+});
+
+request.on('error', error => {
+	console.log(error.message); // eslint-disable-line no-console
+	process.exit(1);
+});
+
+request.end();
