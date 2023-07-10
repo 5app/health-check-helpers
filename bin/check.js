87b7a93a07d37807109d1b74aaa6fcf2e11548b7
Andrew Dodson <andrew@5app.com>
2023-07-10 13:31:51 +0100

    chore(esm): convert to esm

--- bin/check.js
+++ bin/check.js
@@ -1,6 +1,6 @@
 // Inspired by https://blog.sixeyed.com/docker-healthchecks-why-not-to-use-curl-or-iwr/
 
-const http = require('node:http');
+import http from 'node:http';
 
 const {
 	HEALTHCHECK_PORT = 9999,
